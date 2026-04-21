db3a379abe6c5a7abf8e82542f50bfcd391f885b
thiswind <thiswind@users.noreply.github.com>
2026-04-21 12:23:27 +0000

    chore: remove unnecessary files

--- hermes-agent-2026.4.16/web/src/themes/index.ts
+++
@@ -1,3 +0,0 @@
-export { ThemeProvider, useTheme } from "./context";
-export { BUILTIN_THEMES } from "./presets";
-export type { DashboardTheme, ThemeColors, ThemeOverlay, ThemeListResponse } from "./types";
